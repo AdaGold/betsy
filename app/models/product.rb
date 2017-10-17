dd1e36a76f8840c114babee0660575044a289094
Kayla Ecker <The Email Associated with Your Github Account>
2017-10-17 13:20:20 -0700

    testing for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
-end
+
+  it "Successfully loads the index page" do
+
+  end
+
+  it "Successfully loads the show page" do
+
+  end
+
+  it "Successfully loads a new product form page" do
+
+  end
+
+  it "Successfully loads an edit product form page" do
+
+  end
+
+  it "Successfully creates a new work that saves to the database" do
+
+  end
+
+  it "Successfully saves the attributes of a new work" do
+
+  end
+
+  it "Successfully updates attribute for an existing work" do
+
+  end
+  #may do attributes individually and for redirecting
+
+
+
+
+end #desribe products controller
