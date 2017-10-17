021c1e2e4711d0e1a27ec5d57e1a0a8872372196
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-17 13:12:10 -0700

    Add test for products#index

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "must get the index view" do
+    get products_path
+    must_respond_with :success
+  end
 end
