5b532f60a0d3ed7617ce1acc17f2876778536a7e
murog <guillermina.muro@gmail.com>
2017-10-18 15:24:12 -0700

    review belongs to user set as optional

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :product
   validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
   validates :rating, presence: true
