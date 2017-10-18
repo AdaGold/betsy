55a6deadf4d63dc1fb1a85135e441751cff75254
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-18 14:36:56 -0700

    *ran a migration to add a review_text column to the review table, *Added a button_to on the product show page that sends to the review#new, *Filled in the review#new method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,12 @@ class ReviewsController < ApplicationController
   def show
   end
 
+  def new
+    @review = Review.new(product_id: param[:pass_product_id])
+  end
+
   def create
+
   end
 
   def edit
@@ -14,6 +19,5 @@ class ReviewsController < ApplicationController
   def index
   end
 
-  def new
-  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018193825) do
+ActiveRecord::Schema.define(version: 20171018211201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20171018193825) do
     t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "review_text"
     t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
