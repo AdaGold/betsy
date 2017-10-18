d9a6509363b1779466960843a445ea0e11ad90ff
murog <guillermina.muro@gmail.com>
2017-10-18 13:41:36 -0700

    updated validations for category order order_product and product

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,9 @@ class Order < ApplicationRecord
   belongs_to :billing_data
   has_many :entries
   belongs_to :user
+  validates :session_id, presence: true
+  validates :order_status, presence: true
+
+
 
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -2,5 +2,8 @@ class OrderProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
   validates :quantity, presence: true
+  validates :quantity, numericality: { only_integer: true }
+  validates :quantity, numericality: { greater_than: 0 }
+
 
 end
