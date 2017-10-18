71f1c1bdefa8ed653b570e9d4b8ca52d52470bed
Kayla Ecker <The Email Associated with Your Github Account>
2017-10-18 13:47:10 -0700

    beginning testing for oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
 
     @merchant = Merchant.find_by(uid: @auth_hash['uid'], provider: @auth_hash['provider'])
     if @merchant
-      session[:user_id] = @merchant.id
+      session[:merchant_id] = @merchant.id
       flash[:success] = "#{@merchant.username} is logged in!"
     else
       @merchant = Merchant.new uid: @auth_hash['uid'], provider: @auth_hash['provider'], name: @auth_hash['info']['nickname'], email: @auth_hash['info']['email'])
       if @merchant.save
-        session[:user_id] = @merchant.id
+        session[:merchant_id] = @merchant.id
         flash[:success] = "New user: #{@merchant.username} is logged in!"
       else
         flash[:error] = "Unable to save user :( "
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -22,5 +22,7 @@ Minitest::Reporters.use!(
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  # Add more helper methods to be used by all tests here...
+  def setup
+    OmniAuth.config.test_mode = true
+  end
 end
