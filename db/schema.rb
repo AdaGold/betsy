6f7496ed886845655a8c48a354f39e7ec84ece9f
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-18 11:28:54 -0700

    Add migration for a status column in the orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017231027) do
+ActiveRecord::Schema.define(version: 20171018182712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20171017231027) do
     t.integer "billingzip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "products", force: :cascade do |t|
