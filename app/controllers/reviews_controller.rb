f8d423f036c4b3b1da52cb88df4f33495df5d887
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-10-18 13:19:38 -0700

    Reviews controller methods completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,62 @@
 class ReviewsController < ApplicationController
   def index
+    @reviews = Review.order(:id)
   end
 
   def show
+    @review = Review.find_by(id: params[:id].to_i)
+
+    unless @review
+      flash[:error] = "Review not found"
+      redirect_to reviews_path
+    end
   end
+
 
   def edit
+    @review = Review.find_by(id: params[:id].to_i)
+    unless @review
+      redirect_to reviews_path
+    end
   end
+
 
   def update
+    @review = Review.find_by(id: params[:id].to_i)
+
+    if @review.update_attributes review_params
+        redirect_to root_path
+    else
+      render :edit
+    end
   end
 
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new review_params
+
+    if @review.save
+      redirect_to root_path
+    else
+      flash.now[:error] = "Could not create Review"
+      render :new
+    end
   end
 
   def destroy
+    @review = Review.find_by(id: params[:id].to_i)
+    @review.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def review_params
+    return params.require(:review).permit(:user_id, :product_id)
   end
 end
