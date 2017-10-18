af5715f186151e3ddd0c8eabed58744862d3226b
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-10-18 15:50:53 -0700

    created users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
