ad6819344150b00d032b928c0eef7e2fe2c5f29c
murog <guillermina.muro@gmail.com>
2017-10-19 16:57:28 -0700

    added incomplete tests for order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,9 +1,56 @@
-# require "test_helper"
-#
-# describe Order do
-#   let(:order) { Order.new }
-#
-#   it "must be valid" do
-#     value(order).must_be :valid?
-#   end
-# end
+require "test_helper"
+
+describe Order do
+  let(:order) { Order.new }
+  let(:pending_order) { orders(:pending_order)}
+  let(:paid_order)  { orders(:paid_order) }
+  let(:shipped_order) { orders(:shipped_order)}
+  let(:canceled_order) { orders(:canceled_order)}
+
+  describe "relations" do
+    it "has a user" do
+
+    end
+
+    it "has a billing datum if paid, shipped or canceled" do
+
+    end
+
+    it "does not have billing data if pending" do
+
+    end
+    it "should have a collection of order_products" do
+
+    end
+  end
+
+  describe "validations" do
+    it "must have session_id" do
+
+    end
+
+    it "must have an order_status" do
+
+    end
+
+    it "must have an order status of pending, paid, shipped or canceled" do
+
+    end
+
+    it "new orders should have status of pending" do
+    end
+
+    it "rejects invalid order statuses" do
+
+    end
+
+    it "must have a user if authenticated user is logged in" do
+
+    end
+
+    it "must have an associated billing datum if paid, shipped or canceled" do
+
+    end
+
+  end
+end
