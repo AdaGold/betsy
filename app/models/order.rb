7c41050db2dfe09e865357ea46850996cfa4fe36
murog <guillermina.muro@gmail.com>
2017-10-19 16:34:45 -0700

    updated validations on order to make belongs to user as optional.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :billing_data
   has_many :entries
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :session_id, presence: true
   validates :order_status, presence: true
 
