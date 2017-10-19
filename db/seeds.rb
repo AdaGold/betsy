8b8c7f9fb8388d33c67ed4c8bc9c12196a9ed9a8
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-10-19 09:08:14 -0700

    updates and model tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
 require 'csv'
 
 PRODUCT_FILE = Rails.root.join('db', 'seed_data', 'product_seeds.csv')
