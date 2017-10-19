29b6fc86d9f470a9822d6ea97f301f78cddaf6ad
Canaan West <canaan.west@gmail.com>
2017-10-19 14:43:33 -0700

    Adding controller methods to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,20 +4,51 @@ class ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id].to_i)
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product.update_attributes(product_params)
+    if @product.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully updated #{@product.name}"
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not update #{@product.name}"
+      render :edit, status: :not_found
+    end
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully created product #{@product.id} #{@product.name}"
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Unable to create a product"
+      flash[:messages] = @product.errors.messages
+      render :new, status: :bad_request
+    end
   end
 
   def destroy
+    @product.destroy
+    flash[:status] = :success
+    flash[:result_text] = "Successfully destroyed product #{@product.id}, #{@product.name}"
+    redirect_to root_path
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :description, :user, :price)
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -7,12 +7,12 @@ describe ProductsController do
       must_respond_with :success
     end
   end
-  #
-  # it "should get show" do
-  #   get products_show_url
-  #   value(response).must_be :success?
-  # end
-  #
+  
+  it "should get show" do
+    get products_show_url
+    value(response).must_be :success?
+  end
+
   # it "should get edit" do
   #   get products_edit_url
   #   value(response).must_be :success?
