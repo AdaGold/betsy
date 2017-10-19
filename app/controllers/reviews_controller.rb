5953d6476aef8ef9b128b8838c08bf4b868ae931
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-18 20:41:53 -0700

    *added add_a_review button_to on the product show view, *Filled in the new and create method of the reviews_controller, *Added a form the the new review view, *committing current state before debugging (no change done in Atome is showing up on http://localhost:3000/

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,11 +3,17 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    @review = Review.new(product_id: param[:pass_product_id])
+    @review = Review.new(product_id: params[:pass_product_id])
+
   end
 
   def create
-
+    @review = Review.new(product_id: params[:review][:product_id], rating: params[:review][:rating], review_text: params[:review][:review_text])
+    if @review.save
+      redirect_to product_path(@review.product_id)
+    else
+      render :new
+    end
   end
 
   def edit
