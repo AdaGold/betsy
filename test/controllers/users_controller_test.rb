57f7ba31a58813d8ee682ed6b59bfe4ebc9e73a6
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-10-19 14:47:54 -0700

    complete user controller methods and tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,38 +2,45 @@ require "test_helper"
 
 describe UsersController do
   it "should get index" do
-    get users_index_url
-    value(response).must_be :success?
+    get users_path
+    must_respond_with :success
   end
 
   it "should get show" do
-    get users_show_url
-    value(response).must_be :success?
+    get user_path(users(:carl).id)
+    must_respond_with :success
   end
 
-  it "should get edit" do
-    get users_edit_url
-    value(response).must_be :success?
+
+  it "should successfully create a new user" do
+    proc  {
+      post users_path, params: {users: {username: "Test", email: "test@test.com"}}}.must_change 'User.count', 1
+
+      must_respond_with :redirect
+      must_redirect_to root_path
   end
 
-  it "should get update" do
-    get users_update_url
-    value(response).must_be :success?
+  it "should be able to update a user" do
+
+    put user_path(users(:carl)), params: {user: {username: "Karl"} }
+    user = User.find(users(:carl).id)
+    user.username = "Karl"
+    user.username.must_equal "Karl"
+
+    must_respond_with :redirect
+    must_redirect_to root_path
   end
 
-  it "should get new" do
-    get users_new_url
-    value(response).must_be :success?
-  end
 
-  it "should get create" do
-    get users_create_url
-    value(response).must_be :success?
-  end
+  # it "Should be able to delete a user" do
+  #   proc  {
+  #     delete user_path(users(:carl).id)}.must_change 'User.count', - 1
+  #
+  #     must_respond_with :redirect
+  #     must_redirect_to root_path
+  # end
 
-  it "should get destroy" do
-    get users_destroy_url
-    value(response).must_be :success?
-  end
 
+  # it "should get edit" do
+  # end
 end
