23e1f386521e5e7ca865f85a8051acafc754d56e
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-10-19 14:10:06 -0700

    Add tests for remove item from cart

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -122,16 +122,33 @@ describe OrdersController do
   end
 
   describe "#remove_from_cart" do
+    before do
+      get root_path #do this to get session
+      Order.find_by(id: session[:order_id]).orderitems.count.must_equal 0
+    end
+
     it "won't remove an item from an invalid cart" do
       skip
     end
 
     it "won't remove an item if the item doesn't exist in that cart" do
-      skip
+      delete remove_from_cart_path(session[:order_id], product.id)
+      must_respond_with :redirect
+      flash[:status].must_equal :error
+      flash[:result_text].must_equal "This item is not in your cart"
     end
 
     it "will remove a valid item from a valid cart" do
-      skip
+      #add product to cart
+      patch add_order_item_path(session[:order_id], product.id)
+      must_respond_with :success
+      order_item = Order.find_by(id: session[:order_id]).orderitems.first
+      order_item.product_id.must_equal product.id
+      order_item.quantity.must_equal 1
+      delete remove_from_cart_path(session[:order_id], product.id)
+      must_respond_with :redirect
+      flash[:status].must_equal :success
+      flash[:result_text].must_equal "#{product.name} was removed from your cart"
     end
   end
 
