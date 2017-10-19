1e27bc75a0c01e733859a9a389b47414adc12fb5
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-19 16:49:52 -0700

    erase ol code in comments

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,7 @@
 class ReviewsController < ApplicationController
   def show
   end
-
-  # def new
-  #   @review = Review.new(product_id: params[:pass_product_id])
-  #
-  # end
-  #
-  # def create
-  #   @review = Review.new(product_id: params[:review][:product_id], rating: params[:review][:rating], review_text: params[:review][:review_text], title: params[:review][:title])
-  #   if @review.save
-  #     redirect_to product_path(@review.product_id)
-  #   else
-  #     render :new
-  #   end
-  # end
-
+  
   def new
     @review = Review.new(product_id: params[:product_id])
 
