ed32d2780933c6617ffac7df64c00f61b6cf8f97
Shaunna <skwiens@gmail.com>
2017-10-19 10:05:34 -0700

    tests and logout feature added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-
-
-  get 'sessions/create'
-
-  get 'sessions/index'
-
   root to: 'products#index'
 
   resources :products, :except => [:delete]
@@ -13,10 +7,10 @@ Rails.application.routes.draw do
   resources :orders, :except => [:delete]
   resources :reviews, :except => [:delete]
 
-
-  get "/auth/:provider/callback", to: "sessions#create", as: "auth_callback"
-
   patch '/orders/:id/add/:product_id', to: 'orders#add_item', as: 'add_order_item'
   get 'orders/cart', to: 'orders#show_cart', as: 'show_cart'
+
+  get "/auth/:provider/callback", to: "sessions#create", as: "auth_callback"
+  post '/logout', to: 'sessions#logout', as: 'logout'
 
 end
