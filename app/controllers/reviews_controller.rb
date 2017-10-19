c06577d29df5cf0e97a15ccf0f1a15c80f21528c
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-19 15:11:44 -0700

    Changed the new rouut and the new controller to fix rerouting bug after a review fail to save

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,8 +2,22 @@ class ReviewsController < ApplicationController
   def show
   end
 
+  # def new
+  #   @review = Review.new(product_id: params[:pass_product_id])
+  #
+  # end
+  #
+  # def create
+  #   @review = Review.new(product_id: params[:review][:product_id], rating: params[:review][:rating], review_text: params[:review][:review_text], title: params[:review][:title])
+  #   if @review.save
+  #     redirect_to product_path(@review.product_id)
+  #   else
+  #     render :new
+  #   end
+  # end
+
   def new
-    @review = Review.new(product_id: params[:pass_product_id])
+    @review = Review.new(product_id: params[:product_id])
 
   end
 
@@ -12,7 +26,10 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to product_path(@review.product_id)
     else
-      render :new
+      flash[:status] = :failure
+      flash[:message] = "Review failed to be added"
+      flash[:details] = @review.errors.messages
+      redirect_to new_review_path(@review.product_id)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :products, :except => [:delete]
   resources :merchants, :except => [:delete]
   resources :orders, :except => [:delete]
-  resources :reviews, :except => [:delete]
+  resources :reviews, :only => [:create]
+  get "/reviews/:product_id/new", to: "reviews#new", as: "new_review"
 end
