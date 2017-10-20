95036a481adeb458c3ba65be48f18e9146a9c96b
Julia <julia.meier@gmail.com>
2017-10-19 19:14:38 -0700

    outlined categories controller test, added a helper method to the category model to fix the category formatting inputted by the user, created index method in controller. tried testing controller but received a fixture error saying there is no category_id in the products table. will need a lot more work

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,14 @@
 class CategoriesController < ApplicationController
+
+
+  def root
+
+
+  end
+
+  #displays all categories in alphabetical order
   def index
+    @categories = Categories.order(name: :desc)
   end
 
   def show
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,39 +1,71 @@
-# require "test_helper"
-#
-# describe CategoriesController do
-#   it "should get index" do
-#     get categories_index_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get show" do
-#     get categories_show_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get edit" do
-#     get categories_edit_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get update" do
-#     get categories_update_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get new" do
-#     get categories_new_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get create" do
-#     get categories_create_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get destroy" do
-#     get categories_destroy_url
-#     value(response).must_be :success?
-#   end
-#
-# end
+require "test_helper"
+
+describe CategoriesController do
+
+    it "succeeds with all category types" do
+      %w(clothes shoes music).each do |category|
+          Category.by_category(category).length.must_be :>, 0, "No #{category.pluralize} in the test fixtures"
+      end
+
+    get root_path
+    must_respond_with :success
+    end
+
+    it "succeeds with no categories" do
+      Category.destroy_all
+      get root_path
+      must_respond_with :success
+    end
+
+    describe "index" do
+      it "succeeds when there are categories" do
+        Category.count.must_be :>, 0, "There are no categories in the test fixtures"
+        get categories_path
+        must_respond_with :success
+      end
+
+      it "succeeds when there are no categories" do
+        Category.destroy_all
+        get categories_path
+        must_respond_with :success
+      end
+    end
+
+  #only allow auth-users (not guests) should be able to create new categories (vendors)
+  # describe "new" do
+  #   it "allows merchant (i.e. user) to create a new category" do
+  #     get new_category_path
+  #     must_respond_with :success
+  #   end
+      #
+      #it "does not allow guests to create a new category" do
+      #
+      # end
+  # end
+  #
+  # describe "create" do
+  #   it "creates a product with valid data for a real category" do
+  #     product_data = {
+  #       product: {
+  #         name: "test product",
+  #         price: 5.99,
+  #         user_id: 5,
+  #       }
+  #     }
+  #     CATEGORIES.each do |category|
+  #       product_data[:product][:category] = category
+  #       #product.save?
+  #       #product_data.save?
+  #       start_count = Product.count
+  #
+  #       post products_path(category), params: product_data
+  #       must_redirect_to product_path(Product.last)
+  #
+  #       Product.count.must_equal start_count + 1
+  #     end
+  #   end
+  #
+  # end
+
+
+end
