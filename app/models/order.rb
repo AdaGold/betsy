07360855ba06c24bbfba1ad0cdef52fbc0d98123
murog <guillermina.muro@gmail.com>
2017-10-20 12:27:57 -0700

    added methods to validate that order has a valid status and that it has billing datum if not pending

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,19 @@ class Order < ApplicationRecord
   belongs_to :user, optional: true
   validates :session_id, presence: true
   validates :order_status, presence: true
+  validate :has_billing_datum?
+  validate :has_valid_order_status?
 
+  def has_billing_datum?
+    if self.order_status != "pending"
+      self.errors[:billing_datum_id] << "is missing.  Please entire valid billing information." if !(self.billing_datum_id)
+    end
+  end
 
-
+  def has_valid_order_status?
+    valid_statuses = ["pending", "paid", "shipped", "canceled"]
+    if !(valid_statuses.include? self.order_status)
+      self.errors[:order_status] << "is invalid."
+    end
+  end
 end
