d59ae78560009708128f745759783b261125ddf3
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-10-20 13:30:46 -0700

    merging to my local mast

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -34,4 +34,36 @@ describe ProductsController do
     must_respond_with :redirect
     must_redirect_to root_path
   end
+
+  it "should get new" do
+    @merchant = merchants(:sappy1)
+    login(@merchant, :github)
+    get new_product_path
+    must_respond_with :success
+  end
+
+  it "should be able to successfully create a new product" do
+    category_count = Product.pluck(:category).uniq
+    puts "================="
+    puts category_count.inspect
+    puts "================="
+    @merchant = merchants(:sappy1)
+    login(@merchant, :github)
+      proc {
+        post products_path, params: { product: { name: "newbie", price: 2, category: "new category", quantity: 1, merchant_id: merchants(:sappy1).id } }
+      }.must_change 'Product.count', 1
+      must_respond_with :redirect
+      updated_category_count = Product.pluck(:category).uniq
+      updated_category_count.count.must_equal (category_count.count + 1)
+    end
+    it "should rerender the form and not update the review table if it can't create the review" do
+      @merchant = merchants(:sappy1)
+      login(@merchant, :github)
+      proc {
+        post products_path, params: { product: { name: "", price: 2, category: "new category", quantity: 1, merchant_id: merchants(:sappy1).id } }
+      }.must_change 'Review.count', 0
+
+      must_respond_with :redirect
+    end
+
 end
