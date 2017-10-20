928e37b64b16102c70807863e72bebaadca0f1c5
murog <guillermina.muro@gmail.com>
2017-10-20 11:17:47 -0700

    added default order_status of pending to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020175115) do
+ActiveRecord::Schema.define(version: 20171020181604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20171020175115) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string "order_status"
+    t.string "order_status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
