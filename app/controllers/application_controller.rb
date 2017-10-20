d9629e70fd385fb25b4d4b1846fbaf5dc3e4b56b
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-10-20 10:25:36 -0700

    404 method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-<<<<<<< HEAD
+
   def render_404
     raise ActionController::RoutingError.new('Not Found')
   end
-=======
->>>>>>> eea969ce2b5fd8ecbe2d7b10dad27aa46d67156c
+
 
 end
