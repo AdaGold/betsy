c9c00c27f44912ed689e23f56ab4481f6f6fefca
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-10-20 14:49:10 -0700

    created a form to add a new category. Display all categories on category index page and a link_to add new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-<<<<<<< HEAD
+
   def render_404
     raise ActionController::RoutingError.new('Not Found')
   end
-=======
->>>>>>> bedd350556d7491e59030d11bf3d25300e2d80d9
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -79,9 +79,7 @@ ActiveRecord::Schema.define(version: 20171020002826) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "category_id"
     t.bigint "user_id", null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
