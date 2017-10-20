a1a0e741d78cd34b8e51b682f3e33b52edb83bfc
Julia <julia.meier@gmail.com>
2017-10-20 10:29:15 -0700

    outlined categories_controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,7 @@
 class CategoriesController < ApplicationController
 
-
+  #home page shows all categories and a sampling of their products
   def root
-
-
   end
 
   #displays all categories in alphabetical order
@@ -11,21 +9,25 @@ class CategoriesController < ApplicationController
     @categories = Categories.order(name: :desc)
   end
 
+  #show all products in a specific category
   def show
   end
 
+  #makes changes to a category that's already created
   def edit
   end
 
   def update
   end
 
+  #sign-in user can create a new category
   def new
   end
 
   def create
   end
 
+  #signed-in user can delete a category
   def destroy
   end
 end
