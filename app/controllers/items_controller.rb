9e63b20023b6d54bfe068d882e679eb563264a46
Canaan West <canaan.west@gmail.com>
2017-10-23 18:47:44 -0700

    fixing link to remove inventory

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,7 +49,7 @@ class ItemsController < ApplicationController
   def destroy
     if Item.where(product_id: params[:id]).length > 0
       @item = Item.where(product_id: params[:id])
-      @items.last.destroy
+      @item.last.destroy
       redirect_back(fallback_location: root_path)
       flash[:result_text] = "1 #{Product.find(params[:id]).name} removed from inventory!"
     else
